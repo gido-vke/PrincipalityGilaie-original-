cf5ee8a9c8b015c54a6ebac7b73d338d6a372b5e
Gido Verkerke <83137029+gido-vke@users.noreply.github.com>
2021-07-28 19:51:31 +0200

    First Commit

---
+++ script.js
@@ -0,0 +1,31 @@
+var btn2 = document.getElementById('btn2');
+function toggle_visibility() {
+    var e = document.getElementById('ReasoningOFF');
+    if (e.style.display == 'block') {
+        e.style.display = 'none';
+    }
+    else {
+        e.style.display = 'block';
+    }
+    console.log('clicked');
+    return;
+}
+
+//W3 CODE STICKY NAVBAR
+// When the user scrolls the page, execute myFunction
+window.onscroll = function() {myFunction()};
+
+// Get the navbar
+var navbar = document.getElementById("navbar");
+
+// Get the offset position of the navbar
+var sticky = navbar.offsetTop;
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
