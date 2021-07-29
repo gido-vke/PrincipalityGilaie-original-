efbf64787ad0ad8d5facfe2778c73b655733eab4
Gido Verkerke <83137029+gido-vke@users.noreply.github.com>
2021-07-29 16:51:02 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
 var btn2 = document.getElementById('btn2');
 function toggle_visibility() {
-    var e = document.getElementById('ReasoningOFF');
-    if (e.style.display == 'block') {
-        e.style.display = 'none';
-    }
-    else {
-        e.style.display = 'block';
-    }
-    console.log('clicked');
-    return;
+  var e = document.getElementById('ReasoningOFF');
+  if (e.style.display == 'block') {
+    e.style.display = 'none';
+  }
+  else {
+    e.style.display = 'block';
+  }
+  console.log('clicked');
+  return;
 }
 
 //W3 CODE STICKY NAVBAR
 // When the user scrolls the page, execute myFunction
-window.onscroll = function() {myFunction()};
+window.onscroll = function () { myFunction() };
 
 // Get the navbar
 var navbar = document.getElementById("navbar");
@@ -25,7 +25,9 @@ var sticky = navbar.offsetTop;
 function myFunction() {
   if (window.pageYOffset >= sticky) {
     navbar.classList.add("sticky")
+    ScrollFixTED.style.display = 'none';
   } else {
     navbar.classList.remove("sticky");
+    ScrollFixTED.style.display = 'block';
   }
 }
