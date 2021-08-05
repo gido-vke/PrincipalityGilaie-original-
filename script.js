818a742f09758b6184c5e2f1480026cc7dd1f215
Gido Verkerke <83137029+gido-vke@users.noreply.github.com>
2021-08-05 19:45:51 +0200

    commit for style(2).css

--- script.js
+++ script.js
@@ -23,9 +23,6 @@ function grubbel() {
   return;
 }
 
-
-
-
 //W3 CODE STICKY NAVBAR
 // When the user scrolls the page, execute myFunction
 window.onscroll = function () { myFunction() };
@@ -35,6 +32,8 @@ var navbar = document.getElementById("navbar");
 
 // Get the offset position of the navbar
 var sticky = navbar.offsetTop;
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
 
 let ScrollFixTED;
 
